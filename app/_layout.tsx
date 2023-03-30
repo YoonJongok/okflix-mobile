2ba138f0fb48703b5ca3fdc97172035087e70084
Jongok Yoon <yoonjongok1209@gmail.com>
2023-03-30 16:55:14 +0200

    Font and assets preload done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,18 @@
 import { useColorScheme } from "react-native";
 import { ThemeProvider } from "styled-components/native";
 import { darkTheme, lightTheme } from "../theme";
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useAssets } from "expo-asset";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 export default function IndexLayout() {
 	const isDark = useColorScheme() === "dark";
+
+	const [fontsLoaded] = useFonts(FontAwesome5.font);
+	// const [assetsLoaded] = useAssets([require("../assets/images/coins.png")]);
+
+	if (!fontsLoaded) return <SplashScreen />;
 
 	return (
 		<ThemeProvider theme={isDark ? darkTheme : lightTheme}>
